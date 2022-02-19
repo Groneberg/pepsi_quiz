ff38d6cf8c4ae1033dfb6134de4fb02dd6e04983
Stefan Groneberg <stefan.groneberg@gmx.de>
2022-02-19 20:35:52 +0100

    add Start Component

--- src/Components/Start/Start.js
+++ src/Components/Start/Start.js
@@ -1,27 +1,32 @@
 import './Start.css'
 import Header from "../Header/Header";
 
-// import img1 from '../../img/FOP_OTEP-Sustainability_Chickpeatif.png';
-
+import pepsi from '../../img/kisspng-pepsi-max-fizzy-drinks.png';
 
 function Start() {
     return (
-        <div className="taskContainer">
+        <div className="taskContainer startContainer" style={{ backgroundImage: `url(${pepsi})` }}>
             <Header />
-            <div className="start-info">
-                <div id="Bist_Du_ein__Echter__">
-                    <span className="info-1">Bist Du ein </span>
-                    <span className="info-2">Echter</span>
+            <div className="startInfoContainer">
+                <div className="startInfoWrapper">
+                    <p className="info-1">Bist Du ein </p>
+                    <p className="info-2">Echter</p>
                 </div>
-                <div id="Pepsi-Experte">
-                    <span className="info-3">Pepsi-Experte?</span>
+                <div className="startInfoWrapper">
+                    <p className="info-3">Pepsi-Experte?</p>
                 </div>
-                <div id="Zeige_in_3_Antworten__dass_Du_">
-                    <span className="info-4">
+                <div className="startInfoWrapper">
+                    <p className="info-4">
                         Zeige in 3 Antworten, dass Du das Zeug zum
                         Echten Pepsi-Pro hast. Am Ende wartet eine Eiskalte Pepsi auf Dich!
-                    </span>
+                    </p>
                 </div>
+            </div>
+            <div className="continueLinkWrapper">
+                <a className="continueLink" href="/dragDrop">
+                    <span>Ready?</span>
+                    <span>Los geht's</span>
+                </a>
             </div>
         </div>
     );
